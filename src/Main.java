7d18c6a359b2462dc649dd0322a04dd4d3b68f99
YannEscobar <yann.campos.comm@proton.me>
2026-09-04 10:04:50 -0300

    Fix main method implementation and output format

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main{
-  public static void main(String[] args);
-  System.out.println("Sistema Desktop escrito em Java");
+  public static void main(String[] args) {
+    System.out.println("======================");
+    System.out.println("SISTEMA DESKTOP EM JAVA");
+    System.out.println("======================");
+    }
 }
