1a2407a73bfaee86e9c3195133de6e5d9d596240
YannEscobar <yann.campos.comm@proton.me>
2026-09-21 09:44:45 -0300

    Add some variables to use in another methods

---
+++ App/src/model/projeto.java
@@ -0,0 +1,13 @@
+package model;
+import java.util.Scanner;
+
+public class projeto {
+
+    private id = int;
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private Scanner status;
+
+    //Cria uma série de variáveis para ser utilizado por outros métodos e classes
+}
